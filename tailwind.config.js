0d3d10a01ef3b8b9e2fdb8ea8d610768b1959426
sevillaaa <144546166+sevillaaa@users.noreply.github.com>
2025-04-05 22:26:16 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent': '#46A358'
+        'accent': '#46A358',
+        'typo': '#3D3D3D',
+        'bg': '#F5F5F5',
+        'desc': '#727272',
+        'white': '#FFFFFF',
+        'light-green': '#46A3581A'
       },
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif']
